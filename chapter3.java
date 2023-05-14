cfd48492c5e3e77a54f0ebc46c929368f03480a6
Arachne <jhg2819@naver.com>
2023-05-15 00:31:02 +0900

    chapter3

---
+++ chapter3.java
@@ -0,0 +1,33 @@
+public class chapter3 {
+
+	public static void main(String[] args) {
+		int i = 5;
+		System.out.println(i);
+		i++;
+		System.out.println(i);
+		++i;
+		System.out.println(i);
+		
+		int j = i++;
+		System.out.println("i = "+i);
+		int k = ++i;
+		
+		System.out.println("i = "+i);
+		System.out.println("j = "+j);
+		System.out.println("k = "+k);
+		boolean bo = true;
+		if (bo && (i==9)) {
+			System.out.println("YES");
+		} else {
+			System.out.println("NO");
+		}
+		
+		float y = 1__0.1000_000_01F;
+		System.out.println(y);
+		
+		double pi = 3.141592;
+		double shortpi = (int)(pi * 1000 + 0.5) / 1000.0;
+		System.out.println(shortpi);
+	}
+
+}

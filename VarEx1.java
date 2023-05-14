dae03cac0fdd60f66b971bbc1ba401896f3a2500
Arachne <jhg2819@naver.com>
2023-05-14 15:44:03 +0900

    int_temp

--- VarEx1.java
+++ VarEx1.java
@@ -3,15 +3,23 @@ public class VarEx1 {
 	public static void main(String[] args) {
 		int year = 0;
 		int age = 14;
+		int temp = 0;
 		
-		System.out.println(year);
-		System.out.println(age);
+		System.out.println("year = " + year);
+		System.out.println("age = " + age);
 		
 		year = age + 2000;
 		age = age + 1;
 		
-		 System.out.println(year);
-		 System.out.println(age);
+		System.out.println("year = " + year);
+		System.out.println("age = " + age);
+		
+		temp = year;
+		year = age;
+		age = temp;
+		System.out.println("year = " + year);
+		System.out.println("age = " + age);
+		
 
 	}
 

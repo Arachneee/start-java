bc2cad454c612baf65b80e9aa85bb6475358de69
Arachne <jhg2819@naver.com>
2023-05-16 23:52:07 +0900

    card

--- chapter6.java
+++ chapter6.java
@@ -3,18 +3,50 @@ class Tv {
     boolean power;
     int channel;
 
-//    void power();
-//    void channelUp();
-//    void channelDown();
+    void power() {
+        power = !power;
+    }
+    void channelUp(){
+        ++channel;
+    }
+    void channelDown(){
+        --channel;
+    }
 
 }
 
+class Card {
+    String kind;
+    int number;
+    static int width = 100;
+    static int height = 250;
+
+}
 class chapter6 {
     public static void main (String[] args){
-        Tv t1 = new Tv();
-        System.out.println("t1의 channel값은"+ t1.channel + "입니다");
-        t1.channel = 7;
-        System.out.println("t1의 channel값은"+ t1.channel + "입니다");
+        Tv[] tvArr = new Tv[3];
 
+        //객체배열 생성 후 각 요소를 초기화 해야한다.
+        for(int i=0;i< tvArr.length;i++){
+            tvArr[i] = new Tv();
+            tvArr[i].channel = i+10;
+            tvArr[i].color = Integer.toString(i%3);
+        }
+
+        for(Tv tv:tvArr){
+            System.out.println("tv.chanel = " + tv.channel);
+            System.out.println("tv.color = " + tv.color);
+        }
+
+        Card c1 = new Card();
+        c1.kind = "Heart";
+        c1.number = 7;
+
+        Card c2 = new Card();
+        c2.kind = "Space";
+        c2.number = 4;
+
+        System.out.println("c1 = " + c1.kind + " of " + c1.number + " W :" + c1.width + " H " + c1.height);
+        System.out.println("c2 = " + c2.kind + " of " + c2.number + " W :" + c2.width + " H " + c2.height);
     }
 }

beb8dd0e7d9adaddf0e498f22ac4eb868e00c03d
Arachne <jhg2819@naver.com>
2023-05-18 00:34:02 +0900

    6-18

--- Exercise6_20.java
+++ Exercise6_20.java
@@ -4,7 +4,7 @@ class Exercise6_20
     static int[] shuffle(int [] arr){
         int n = arr.length;
         for(int i=0;i<n;i++){
-            int var = (int)(Math.random()*(n-1));
+            int var = (int)(Math.random()*n);
             int temp = arr[var];
             arr[var] = arr[i];
             arr[i] = temp;
@@ -14,8 +14,8 @@ class Exercise6_20
     public static void main(String[] args)
     {
         int[] original = {1,2,3,4,5,6,7,8,9};
-        System.out.println(java.util.Arrays.toString(original));
+        System.out.println(Arrays.toString(original));
         int[] result = shuffle(original);
-        System.out.println(java.util.Arrays.toString(result));
+        System.out.println(Arrays.toString(result));
     }
 }

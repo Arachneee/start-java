8dc689ac97c3ed2736b385b799729a288fe83c7d
Arachne <jhg2819@naver.com>
2023-05-18 21:43:40 +0900

    chapter7 start

---
+++ Timetest.java
@@ -0,0 +1,24 @@
+public class Timetest{
+    public static void main(String args[]){
+        Time t = new Time(30);
+        System.out.println(t);
+        t.setHour(t.getHour()+1);
+        System.out.println(t);
+    }
+}
+class Time {
+    private int hour;
+
+    Time(int hour){
+        setHour(hour);
+    }
+
+    public int getHour() { return hour;}
+    public void setHour(int hour){
+        if (hour < 0 || hour > 23) return;
+        this.hour = hour;
+    }
+    public String toString(){
+        return hour + ":00";
+    }
+}

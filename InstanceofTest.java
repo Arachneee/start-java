51164f5f0c36a8b26a33e5b8b75029374c66297d
Arachne <jhg2819@naver.com>
2023-05-19 00:18:37 +0900

    chapter7 instanceof

---
+++ InstanceofTest.java
@@ -0,0 +1,16 @@
+public class InstanceofTest {
+
+    public static void main(String[] args){
+        FireEngine fe = new FireEngine();
+        Car fe2 = null;
+
+        if(fe instanceof Car){
+            fe2 = (Car)fe;
+        }
+
+        System.out.println(fe2.getClass().getName());
+    }
+
+}
+class Car {}
+class FireEngine extends Car {}

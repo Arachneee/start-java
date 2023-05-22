6ca56cf6d833bcaa7d6f11bc8d257bc246c2820b
Arachne <jhg2819@naver.com>
2023-05-22 21:01:37 +0900

    Exercise9_14

--- Main.java
+++ Main.java
@@ -1,2 +1,22 @@
-public class Main {
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+
+public class Main{
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+
+        while(true){
+
+        }
+
+    }
 }

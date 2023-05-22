871f667802fd849fe095299c633182a308acc37a
Arachne <jhg2819@naver.com>
2023-05-22 19:58:54 +0900

    Exercise9_4

--- chapter9/Exercise9_4.java
+++ chapter9/Exercise9_4.java
@@ -1,4 +1,19 @@
 package chapter9;
 
-public class Exercise9_4 {
+class Exercise9_4 {
+    static void printGraph(int[] dataArr, char ch) {
+        for(int i:dataArr){
+            for(int j=0;j<i;j++){
+                System.out.print(ch);
+            }
+            System.out.println(i);
+        }
+/*
+(1) printGraph . 메서드를 작성하시오
+*/
+    }
+    public static void main(String[] args) {
+        printGraph(new int[]{3,7,1,4},'*');
+    }
 }
+

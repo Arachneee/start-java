7f5a9d8509699dcde2a808c639860fa57f8501d1
Arachne <jhg2819@naver.com>
2023-05-24 00:08:17 +0900

    chapter11-1

--- chapter11/ArrayListEx1.java
+++ chapter11/ArrayListEx1.java
@@ -10,9 +10,22 @@ public class ArrayListEx1 {
         list1.add(3);
 
         for(int i=list1.size()-1;i>=0;i--){
-            System.out.println(i);
+            System.out.print(i+": before"+list1);
             list1.remove(i);
-            System.out.println(list1);
+            System.out.println("after"+list1);
         }
+
+        ArrayList list2 = new ArrayList(10);
+        list2.add('1');
+        list2.add('a');
+        list2.add("asbd");
+
+        System.out.println(list2);
+        if (list2.get(2).toString().charAt(0) == 'a'){
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
     }
 }

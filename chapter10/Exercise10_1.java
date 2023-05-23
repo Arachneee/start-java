b0caf1bbf1515fc54954fa9b3deaaa20d6c77df5
Arachne <jhg2819@naver.com>
2023-05-23 19:55:55 +0900

    chapter10_1

--- chapter10/Exercise10_1.java
+++ chapter10/Exercise10_1.java
@@ -8,17 +8,19 @@ public class Exercise10_1 {
     public static void main(String[] args){
         Calendar d = Calendar.getInstance();
         d.set(Calendar.YEAR,2010);
-        d.set(Calendar.MONTH,0);
-        d.set(Calendar.DATE,1);
 
-        System.out.println(6-d.get(Calendar.DAY_OF_WEEK));
-
-        System.out.println(toString(d));
+        for(int i=0;i<12;i++){
+            d.set(Calendar.MONTH,i);
+            d.set(Calendar.DATE,1);
+            d.add(Calendar.DATE,7-d.get(Calendar.DAY_OF_WEEK)+1);
+            d.add(Calendar.WEDNESDAY,1);
+            System.out.println(toString(d));
+        }
     }
 
     public static String toString(Calendar date){
 
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd은 2번째 일요일입니다.");
 
         return sdf.format(date.getTime());
     }

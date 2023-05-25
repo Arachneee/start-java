96a01afbb11214bb55949ff4073b2bbe4a4b0b8f
Arachne <jhg2819@naver.com>
2023-05-25 20:25:44 +0900

    exercise 11_13

--- chapter11/Exercise11_12.java
+++ chapter11/Exercise11_12.java
@@ -2,15 +2,38 @@ package chapter11;
 
 import java.util.*;
 class Exercise11_12 {
-    public static void main(String args[]) throws Exception {
+    public static void main(String args[]) throws Exception
+    {
         SutdaDeck deck = new SutdaDeck();
         deck.shuffle();
-        Player p1 = new Player("타짜", deck.pick(), deck.pick());
-        Player p2 = new Player("고수", deck.pick(), deck.pick());
-        System.out.println(p1+" "+deck.getPoint(p1));
-        System.out.println(p2+" "+deck.getPoint(p2));
+        Player[] pArr = {
+                new Player("타짜", deck.pick(), deck.pick()),
+                new Player("고수", deck.pick(), deck.pick()),
+                new Player("물주", deck.pick(), deck.pick()),
+                new Player("중수", deck.pick(), deck.pick()),
+                new Player("하수", deck.pick(), deck.pick())
+        };
+        TreeMap rank = new TreeMap(new Comparator(){
+            public int compare(Object o1, Object o2) {
+                Player p1 = (Player) o1;
+                Player p2 = (Player) o2;
+
+                return p2.point-p1.point;
+/*
+(1) . 알맞은 코드를 넣어 완성하시오
+*/
+            }
+        });
+        for(int i=0; i < pArr.length;i++) {
+            Player p = pArr[i];
+            rank.put(p, deck.getPoint(p));
+            System.out.println(p+" "+deck.getPoint(p));
+        }
+        System.out.println();
+        System.out.println("1 "+rank.firstKey()+"위는 입니다.");
     }
 }
+
 class SutdaDeck
 {
     final int CARD_NUM = 20;
@@ -27,51 +50,33 @@ class SutdaDeck
     }
     void registerJokbo() {
         jokbo.put("KK", 4000);
-        jokbo.put("1010",3100);
-        jokbo.put("99", 3090);
-        jokbo.put("88", 3080);
-        jokbo.put("77", 3070);
-        jokbo.put("66", 3060);
-        jokbo.put("55", 3050);
-        jokbo.put("44", 3040);
-        jokbo.put("33", 3030);
-        jokbo.put("22", 3020);
-        jokbo.put("11", 3010);
-        jokbo.put("12", 2060);
-        jokbo.put("21", 2060);
-        jokbo.put("14", 2050);
-        jokbo.put("41", 2050);
-        jokbo.put("19", 2040);
-        jokbo.put("91", 2040);
-        jokbo.put("110", 2030);
-        jokbo.put("101", 2030);
-        jokbo.put("104", 2020);
-        jokbo.put("410", 2020);
+        jokbo.put("1010",3100); jokbo.put("12", 2060);
+        jokbo.put("99", 3090); jokbo.put("21", 2060);
+        jokbo.put("88", 3080); jokbo.put("14", 2050);
+        jokbo.put("77", 3070); jokbo.put("41", 2050);
+        jokbo.put("66", 3060); jokbo.put("19", 2040);
+        jokbo.put("55", 3050); jokbo.put("91", 2040);
+        jokbo.put("44", 3040); jokbo.put("110", 2030);
+        jokbo.put("33", 3030); jokbo.put("101", 2030);
+        jokbo.put("22", 3020); jokbo.put("104", 2020);
+        jokbo.put("11", 3010); jokbo.put("410", 2020);
         jokbo.put("46", 2010);
         jokbo.put("64", 2010);
-
     }
     int getPoint(Player p) {
         if(p==null) return 0;
         SutdaCard c1 = p.c1;
         SutdaCard c2 = p.c2;
         Integer result = 0;
-
-        if (c1.isKwang && c2.isKwang) result = (Integer)jokbo.get("KK");
-        else if (jokbo.containsKey(c1.num + ""+c2.num)) result = (Integer)jokbo.get(c1.num + ""+c2.num);
-        else {
-            result = (Integer) ((c1.num + c2.num) % 10 + 1000);
+        if(c1.isKwang && c2.isKwang) {
+            result = (Integer)jokbo.get("KK");
+        } else {
+            result = (Integer)jokbo.get(""+c1.num+c2.num);
+            if(result==null) {
+                result = (Integer)((c1.num + c2.num) % 10 + 1000);
+            }
         }
-        p.point = result;
-
-/*
-(2) . 아래의 로직에 맞게 코드를 작성하시오
-1. , jokbo "KK" . 카드 두 장이 모두 광이면 에서 키를 로 해서 점수를 조회한다
-2. (num) jokbo . 두 카드의 숫자 로 에서 등급을 조회한다
-3. , . 해당하는 등급이 없으면 아래의 공식으로 점수를 계산한다
-(c1.num + c2.num) % 10 + 1000
-4. Player (point) . 의 점수 에 계산한 값을 저장한다
-*/
+        p.point = result.intValue();
         return result.intValue();
     }
     SutdaCard pick() throws Exception {
@@ -93,12 +98,12 @@ class SutdaDeck
             cards[j] = tmp;
         }
     }
-}
+} // SutdaDeck
 class Player {
     String name;
     SutdaCard c1;
     SutdaCard c2;
-    int point; // - 카드의 등급에 따른 점수 새로 추가
+    int point;
     Player(String name, SutdaCard c1, SutdaCard c2) {
         this.name = name ;
         this.c1 = c1 ;

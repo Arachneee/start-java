abae1226fcd224d24a0670b7a7656eeaa427158c
Arachne <jhg2819@naver.com>
2023-05-27 00:15:21 +0900

    chapter14

---
+++ chapter14/test.java
@@ -0,0 +1,24 @@
+package chapter14;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+public class test {
+    public static void main(String[] args){
+        List<Integer> arr = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+        //List<Integer> arr = Arrays.asList(1,2,3,4,5);
+
+        arr.replaceAll(i->i*10);
+
+        Stream<Integer> intStream = arr.stream();
+
+        arr.add(6);
+
+
+        intStream.forEach(System.out::println);
+
+        boolean ok = arr instanceof ArrayList<Integer>;
+        System.out.println(ok);
+
+    }
+}

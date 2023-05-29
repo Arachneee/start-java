5d9b502bca58b3fb9d3d403f030515c5350d64cd
Arachne <jhg2819@naver.com>
2023-05-29 20:19:39 +0900

    Exercise16-1

--- chapter16/test.java
+++ chapter16/test.java
@@ -1,4 +1,27 @@
 package chapter16;
 
+import java.util.*;
+import java.util.stream.*;
+
 public class test {
+    public static void main(String[] args){
+        String ip = "192.168.10.100";
+        String subnet = "255.255.255.0";
+
+        int[] ipArr = Stream.of(ip.split("\\.")).mapToInt(Integer::parseInt).toArray();
+        //System.out.println(Arrays.toString(ipArr));
+
+        int[] subnetArr = Stream.of(subnet.split("\\.")).mapToInt(Integer::parseInt).toArray();
+        //System.out.println(Arrays.toString(subnetArr));
+
+        StringBuffer sbip = new StringBuffer("네트워크 주소:");
+        StringBuffer hostip = new StringBuffer("호스트 주소:");
+
+        for(int i=0;i<ipArr.length;i++){
+            sbip.append((ipArr[i] & subnetArr[i])+".");
+            hostip.append((ipArr[i] & ~subnetArr[i])+".");
+        }
+        System.out.println(sbip);
+        System.out.println(hostip);
+    }
 }
